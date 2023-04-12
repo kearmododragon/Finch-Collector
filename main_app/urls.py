dd031b6b823ac4d87b2dc96a372a246ee51d732c
kearmododragon <ckearney1992@gmail.com>
2023-04-12 16:56:31 +0200

    added table and pages for n64 games

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path("about", views.about, name='about'),
-  path("n64_games", views.allfinches, name = 'n64_games')
+  path("n64_games", views.allfinches, name = 'n64_index')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+n64 = [
+  {"name": "Mario Kart 64", "genre": "Racing", "franchise": "Mario"},
+  {"name": "Pokemon Stadium", "genre": "stratergy", "franchise": "Pokemon"},
+  ]
 
 # Define the home view
 def home(request):
@@ -9,4 +13,4 @@ def about(request):
   return render(request, 'about.html')
 
 def allfinches(request):
-  return render(request, 'n64_games.html')
+  return render(request, 'n64_index.html', {'n64': n64})
